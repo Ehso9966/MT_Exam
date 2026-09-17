8f7f15934eba667ecffd84dbbb24438dff1486bc
ThuRain <ehso9966@gmail.com>
2026-09-17 16:51:01 +0900

    Fix duplicate btnSeoLang IDs: app header uses btnLangToggle, landing header uses btnSeoLang

--- js/app.js
+++ js/app.js
@@ -242,7 +242,7 @@ MT.App = (function () {
   function bindHeader() {
     const $ = function (id) { return document.getElementById(id); };
 
-    const langBtn = $('btnSeoLang');
+    const langBtn = $('btnLangToggle');
     if (langBtn) langBtn.addEventListener('click', function () {
       MT.I18n.toggle();
       render();
